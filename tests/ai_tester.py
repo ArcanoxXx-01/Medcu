e1a317cf94f004d6881d6af789999945fa9d63a4
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-06-23 09:42:40 -0400

    update tester

--- tests/ai_tester.py
+++ tests/ai_tester.py
@@ -4,64 +4,117 @@ from tabnanny import verbose
 from google import genai
 from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.getenv('GEMINI_API_KEY', None)
+from console_app import Asisstant
+from processor import FireworksProcessor
+from app.config import *
+# load_dotenv()
+# API_KEY = os.getenv('FIREWORKS_API_KEY', None)
 
-if not API_KEY:
-    raise ValueError("GEMINI_API_KEY not found in .env")
+# if not API_KEY:
+#     raise ValueError("GEMINI_API_KEY not found in .env")
 
-client = genai.Client(api_key=API_KEY)
+# client = genai.Client(api_key=API_KEY)
 
-with open('tests.json', 'r', encoding='utf-8') as f:
-    tests = json.load(f)
+class Tester(Asisstant):
+    def __init__(self):
+        super().__init__()
+        self.answer = ''
+        self.boolean_prompt = ''
+ 
+        self.llm = FireworksProcessor(
+            model_id=FIREWORKS_MODEL_ID,
+            api_key=FIREWORKS_API_KEY,
+            api_url=API_URL
+        )
 
-for key, value in tests.items():
-    patient = value['Consulta']
-    diagnostic = value['Diagnóstico']
+    def generar_respuesta_error(self, mensaje):
+        pass
 
-    medical_prompt = (
-        f"Eres un médico clínico. "
-        f"A continuación, se te presenta una consulta de un paciente y un diagnóstico inicial. "
-        f"Tu tarea es generar un diagnóstico más detallado, inferido o corregido si fuese necesario.\n\n"
-        f"Consulta del paciente:\n{patient}\n\n"
-        f"Diagnóstico inicial:\n{diagnostic}\n\n"
-        f"Diagnóstico final:"
-    )
+    def generar_respuesta_diagnostico(self, diagnostico, entidades):
+        self.answer = self.process_model.generate_diagnostic(diagnostico, entidades)
+        
+    def gen_boolean_prompt(self, patient, patient_prompt):
+        self.boolean_prompt = (
+            f"Actúa como la persona que describes en el siguiente perfil:\n{patient_prompt}\n\n"
+            f"Padeces de los siguientes síntomas:\n{patient}\n\n"
+            f"Responde únicamente con 1 o 0 a las preguntas médicas relacionadas con estos síntomas."
+        )
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=medical_prompt
-    )
-    
-    expected_answer = response.text
+    def preguntar_usuario(self, question):
+        response = self.llm._llm_request(self.boolean_prompt + question, temperature=0.1)
+        # response = client.models.generate_content(
+        #     model="gemini-2.5-flash",
+        #     contents=self.boolean_prompt + question
+        # )
 
-    patient_prompt = (
-        f"Eres un paciente con las siguientes características: {patient}.\n"
-        f"Asistes a una consulta para atender tus padecimientos.\n"
-        f"Comenta brevemente tus padecimientos al médico que te atenderá."
-    )
+        self.confirmation = response.text[-1] == '1'
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=patient_prompt
-    )
+    def test(self):
+        with open('tests.json', 'r', encoding='utf-8') as f:
+            tests = json.load(f)
 
-    first_message = response.text
+        accuracy = 0; cnt_tests = 0
+        for key, value in tests.items():
+            patient = value['Consulta']
+            diagnostic = value['Diagnóstico']
 
-    boolean_prompt = (
-        f"Actúa como la persona que describes en el siguiente perfil:\n{patient_prompt}\n\n"
-        f"Padeces de los siguientes síntomas:\n{patient}\n\n"
-        f"Responde únicamente con 1 o 0 a las preguntas médicas relacionadas con estos síntomas."
-    )
+            medical_prompt = (
+                f"Eres un médico clínico. "
+                f"A continuación, se te presenta una consulta de un paciente y un diagnóstico inicial. "
+                f"Tu tarea es generar un diagnóstico más detallado, inferido o corregido si fuese necesario.\n\n"
+                f"Consulta del paciente:\n{patient}\n\n"
+                f"Diagnóstico inicial:\n{diagnostic}\n\n"
+                f"Diagnóstico final:"
+            )
 
-    print(first_message)
+            response = self.llm._llm_request(medical_prompt, temperature=0.0)
 
-    query = input()
+            # response = client.models.generate_content(
+            #     model="gemini-2.5-flash",
+            #     contents=medical_prompt
+            # )
+            
+            expected_answer = response.text
 
-    followup_response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=boolean_prompt + query
-    )
-    
-    print(followup_response.text[-1])
-    
+            patient_prompt = (
+                f"Eres un paciente con las siguientes características: {patient}.\n"
+                f"Asistes a una consulta para atender tus padecimientos.\n"
+                f"Comenta brevemente tus padecimientos al médico que te atenderá."
+            )
+
+            response = self.llm._llm_request(patient_prompt, temperature=0.4)
+
+            # response = client.models.generate_content(
+            #     model="gemini-2.5-flash",
+            #     contents=patient_prompt
+            # )
+
+            first_message = response.text
+
+            self.gen_boolean_prompt(patient, patient_prompt)
+            self.orchestrator.diagnosticar(first_message)
+
+            evaluator_prompt = (
+                f"Actúa como un evaluador médico experto. "
+                f"Tu tarea es calificar la calidad del diagnóstico realizado por un doctor, comparándolo con un diagnóstico esperado. "
+                f"Otorga una puntuación entre 0 y 5 siguiendo esta escala:\n"
+                f"5: Diagnóstico completamente correcto, preciso y bien argumentado.\n"
+                f"4: Diagnóstico mayormente correcto, con pequeños errores o omisiones.\n"
+                f"3: Diagnóstico parcialmente correcto, incluye elementos clave pero también errores importantes.\n"
+                f"2: Diagnóstico en su mayoría incorrecto, pero con una idea lejana o vaga relacionada.\n"
+                f"1: Diagnóstico equivocaddo, sin relación clara con el caso, pero sin riesgos críticos.\n"
+                f"0: Diagnóstico completamente incorrecto y potencialmente peligroso.\n"
+                f"Diagnóstico esperado: {expected_answer}"
+                f"Diagnóstico a evaluar: {self.answer}"
+            )
+
+            response = self.llm._llm_request(evaluator_prompt, temperature=0.1)
+            # response = client.models.generate_content(
+            #     model="gemini-2.5-flash",
+            #     contents=evaluator_prompt
+            # )          
+
+            accuracy += int(response.text.split()[-1])
+            cnt_tests += 5
+
+        return accuracy / cnt_tests
